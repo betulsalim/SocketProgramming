d9e1d1c20439bc9a03796a0285c5cd01a1189212
Betül Salim <112887429+betulsalim@users.noreply.github.com>
2022-12-31 14:52:03 +0300

    Add files via upload

---
+++ SocketProggramming/src/main/java/com/example/socketproggramming/User.java
@@ -0,0 +1,12 @@
+package com.example.socketproggramming;
+
+import java.util.ArrayList;
+
+public class User {
+    public String nameString;
+    public String fullNameString;
+    public String passwordString;
+    public String emailString;
+    public String genderString;
+    public String phoneNumberString;
+}
